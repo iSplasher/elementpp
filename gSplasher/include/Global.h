7b5693a21b6d3e4886cf1ee775d850f2ad54faa0
iSplasher <isplasher@gmail.com>
2016-02-02 22:58:29 +0100

    Progress on window windows implementation;

--- gSplasher/include/Global.h
+++ gSplasher/include/Global.h
@@ -10,6 +10,7 @@
 
 #define GSP_VERSION_MAJOR 0
 #define GSP_VERSION_MINOR 1
+#define GSP_VERSION_PATCH 0
 #define USING_NAMESPACE using namespace gsp;
 #define NAMESPACE_BEGIN namespace gsp {
 #define NAMESPACE_END }
@@ -21,7 +22,28 @@
 //#else
 //	#define GSPLASHER_API __declspec(dllimport)
 //#endif
-#define GSPLASHER_API 
+#define GSPLASHER_API
+
+/// <summary>
+/// Define system
+/// </summary>
+#if defined(_WIN32)
+
+	#define OS_WINDOWS
+	// needed to avoid issues when including windows specific header
+	#ifndef NOMINMAX
+		#define NOMINMAX
+	#endif
+
+#elif defined(__APPLE__) && defined(__MACH__)
+
+	#define OS_MAC
+
+#elif defined(__linux__)
+
+	#define OS_LINUX
+
+#endif
 
 NAMESPACE_BEGIN
 
